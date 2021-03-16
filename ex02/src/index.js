228983996a45039cd3f6b279d88994ad52edd419
sara-gheith <saraannegheith@gmail.com>
2021-03-16 19:08:20 +0100

    Boolean Values

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+
+function myFunction(myParameter) {
+  if (myParameter) {
+      return "The parameter is true!";
+  }
+  return "The parameter is fales!";
+}
+myFunction(true);
+myFunction(fales);
+// Only change code above this line
+
+module.exports = myFunction;
