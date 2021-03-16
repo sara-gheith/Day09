905545bc431f8448ebb5155d0171c209f9e17578
sara-gheith <saraannegheith@gmail.com>
2021-03-16 22:05:14 +0100

    If Else Statements

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,11 +1,13 @@
 // Only change code below this line
 
 function functionElse(num) {
+  var result;
   if (num <= 5) {
     return "5 or smaller";
   } else {
     return "Bigger than 5";
   }
+  return result;
 }
 
 console.log(functionElse(4));
